e6171db11f403fc6927830b067eb2e3cad6ae270
Cronna-soft <cronna2322@gmail.com>
2025-07-02 19:24:42 -0500

    Modificación de archivos para el despliegue en github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router"
+import { HashRouter as Router, Routes, Route } from "react-router"
 import { Navbar, FloatingButtons, FooterSection } from './components'
 import { HomePage, ProductDetail, CatalogPage, Gracias, ContactPage, Lookbook } from './pages'
 
